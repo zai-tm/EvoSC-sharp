cbce141121b1b1f57e92f22661397fdfac82efdc
Kyreus <admin@marco-hajek.me>
2022-12-20 17:32:25 +0100

    [71_Localization] added localization example

--- examples/SimpleModule/SimpleModule.cs
+++ examples/SimpleModule/SimpleModule.cs
@@ -1,4 +1,8 @@
-﻿using EvoSC.Modules;
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using EvoSC.Common.Config.Models;
+using EvoSC.Modules;
 using EvoSC.Modules.Attributes;
 using EvoSC.Modules.Interfaces;
 using EvoSC.Modules.Official.Player.Interfaces;
@@ -9,9 +13,22 @@ namespace SimpleModule;
 [Module]
 public class SimpleModule : EvoScModule, IToggleable
 {
+    private IEvoScBaseConfig _config;
+    private ILogger<SimpleModule> _logger;
+    private ResourceManager _resManager;
+    private CultureInfo _cultureInfo;
+
+    public SimpleModule(IEvoScBaseConfig config, ILogger<SimpleModule> logger)
+    {
+        _config = config;
+        _logger = logger;
+    }
+
     public Task Enable()
     {
-        Console.WriteLine("Hello World!");
+        _resManager = new ResourceManager("SimpleModule.Localization", typeof(SimpleModule).Assembly);
+        _cultureInfo = _config.Localization.CultureInfo;
+        _logger.LogDebug(_resManager.GetString("SimpleModule.helloworld", _cultureInfo));
         return Task.CompletedTask;
     }
 
